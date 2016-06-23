d5c828d5b7b69c4c8f4d9a29a9ae8ee5c993206a
kristianos <albert_93_albert@yahoo.com>
2016-06-23 17:22:13 +0100

    Second commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'Javascript: The Good Parts' },
+    { title: 'Harry Potter 1'},
+    { title: 'Eloquent Ruby' },
+    { title: 'The Dark Tower'}
+  ]
+}
